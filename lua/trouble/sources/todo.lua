a8ab13f008256eef9502a5a941b13afc9ebc4d32
abhayk <abhaykrishnan87@gmail.com>
2025-03-01 10:59:45 +0530

    Setting up the calculation of days remaining and its display

--- lua/trouble/sources/todo.lua
+++ lua/trouble/sources/todo.lua
@@ -15,6 +15,12 @@ M.config = {
         hl = "TodoFg" .. ctx.item.tag,
       }
     end,
+    days_remaining_str = function(ctx)
+      return {
+        text = ctx.item.days_remaining_str,
+        hl = "TodoBg" .. "WARN"
+      }
+    end,
   },
   modes = {
     todo = {
@@ -26,7 +32,7 @@ M.config = {
         { "filename", format = "{file_icon} {filename} {count}" },
       },
       sort = { { buf = 0 }, "filename", "pos", "message" },
-      format = "{todo_icon} {text} {pos}",
+      format = "{todo_icon} {text} {pos} {days_remaining_str}",
     },
   },
 }
@@ -37,6 +43,26 @@ function M.get(cb)
     for _, item in pairs(results) do
       local row = item.lnum
       local col = item.col - 1
+
+
+      local days_remaining = 10000
+      local date_str = string.match(item.text, "%[(%d%d%-%d%d%-%d%d%d%d)%]")
+      if date_str then
+        local day, month, year = date_str:match("(%d%d)%-(%d%d)%-(%d%d%d%d)")
+        day = tonumber(day)
+        month = tonumber(month)
+        year = tonumber(year)
+        if day and month and year then
+          local due_date = os.time({year = year, month = month, day = day, hour = 0, min = 0, sec = 0})
+          local now = os.time({year = os.date("%Y"), month = os.date("%m"), day = os.date("%d"), hour = 0, min = 0, sec = 0})
+          local diff_in_seconds = due_date - now
+          local remaining = math.ceil(diff_in_seconds / (24 * 3600))
+          days_remaining = remaining
+        end
+      end
+
+      local days_remaining_str = (days_remaining == 10000) and "" or "(" .. days_remaining .. " days remaining)"
+
       items[#items + 1] = Item.new({
         buf = vim.fn.bufadd(item.filename),
         pos = { row, col },
@@ -45,6 +71,8 @@ function M.get(cb)
         filename = item.filename,
         item = item,
         source = "todo",
+        days_remaining = days_remaining,
+        days_remaining_str = days_remaining_str
       })
     end
     cb(items)
